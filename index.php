c2fd4d44540a88d856ca86b8c9f2a1d2dc1312dc
scontro <hilla.fred@helsinki.fi>
2014-05-18 20:46:16 +0300

    first week

---
+++ index.php
@@ -0,0 +1,10 @@
+
+<?php 
+?><!DOCTYPE HTML>
+<html>
+<head><title><?php echo "Fuksipassi 2014" ?></title></head>
+<body>
+  <h1><?php echo "Tulossa!" ?></h1>
+  <p><li><a href="https://hihihi.users.cs.helsinki.fi/Fuksipassi-2014/esittelysivu.html">Työn esittelysivu</a></li>
+</body>
+</html>
