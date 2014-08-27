72ce5effbb1c27d1d99083dffd069047fecfe62c
scontro <hilla.fred@helsinki.fi>
2014-08-28 00:18:42 +0300

    möbiili

--- views/frame.php
+++ views/frame.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/bootstrap.css" rel="stylesheet">
         <link href="css/bootstrap-theme.css" rel="stylesheet">
         <link href="css/main.css" rel="stylesheet">
